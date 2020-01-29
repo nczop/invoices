a0f6a0793cd338b0347937794adcfd4f071f909a
Michał Maciorowski <mmaciorowski@pgs-soft.com>
2020-01-29 11:41:46 +0100

    adds json server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const jsonServer = require("json-server");
+const routes = require('./routes.json');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.rewriter(routes));
+server.use(router);
+
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
